375a37a566b63309f0a0c457a7a8d2a5f93138b9
Laura Docherty <laurahdocherty@gmail.com>
2020-07-08 18:16:08 -0500

    Created if add statement

--- calculator.py
+++ calculator.py
@@ -6,7 +6,14 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 while True:
     input_string = input('Enter your equation > ')
-    tokens = input_string.split()
-    if tokens.startswith('q'):
+    tokens = input_string.split(' ')
+    if tokens[0] == 'q':
         break
-
+    else:
+        if tokens[0] == '+':
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
+            print(add(num1, num2))
+        if tokens[0] == '-':
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
