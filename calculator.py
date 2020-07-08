bf33e4451b3d90f64982780008ccf0dd3d21fa86
Laura Docherty <laurahdocherty@gmail.com>
2020-07-08 18:35:17 -0500

    Created if divide, square, cube, power, mod if statements

--- calculator.py
+++ calculator.py
@@ -10,19 +10,21 @@ while True:
     if tokens[0] == 'q':
         break
     else:
+        num1 = int(tokens[1])
+        num2 = int(tokens[2])
         if tokens[0] == '+':
-            num1 = int(tokens[1])
-            num2 = int(tokens[2])
             print(add(num1, num2))
         if tokens[0] == '-':
-            num1 = int(tokens[1])
-            num2 = int(tokens[2])
             print(subtract(num1, num2))
         if tokens[0] == '*':
-            num1 = int(tokens[1])
-            num2 = int(tokens[2])
             print(multiply(num1, num2))
         if tokens[0] == '/':
-            num1 = int(tokens[1])
-            num2 = int(tokens[2])
             print(divide(num1, num2))
+        if tokens[0] == 'square':
+            print(square(num1))
+        if tokens[0] == 'cube':
+            print(cube(num1))
+        if tokens[0] == 'pow':
+            print(power(num1, num2))
+        if tokens[0] == 'mod':
+            print(mod(num1, num2))
