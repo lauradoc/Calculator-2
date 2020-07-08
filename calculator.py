ce47a0315ced57e6a61af7096f83eb77a2b2756d
Laura Docherty <laurahdocherty@gmail.com>
2020-07-08 17:45:56 -0500

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+"""CLI application for a prefix-notation calculator."""
+
+from arithmetic import (add, subtract, multiply, divide, square, cube,
+                        power, mod, )
+
+
+while True:
+    input_string = input('Enter your equation > ')
