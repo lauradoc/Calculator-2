1adaf955c0ab26d97d6c24c6269bf4249ae1365b
Laura Docherty <laurahdocherty@gmail.com>
2020-07-08 18:56:42 -0500

    Clarified if list takens in 2 or 3 arguments

--- calculator.py
+++ calculator.py
@@ -11,20 +11,22 @@ while True:
         break
     else:
         num1 = int(tokens[1])
-        num2 = int(tokens[2])
-        if tokens[0] == '+':
-            print(add(num1, num2))
-        if tokens[0] == '-':
-            print(subtract(num1, num2))
-        if tokens[0] == '*':
-            print(multiply(num1, num2))
-        if tokens[0] == '/':
-            print(divide(num1, num2))
-        if tokens[0] == 'square':
-            print(square(num1))
-        if tokens[0] == 'cube':
-            print(cube(num1))
-        if tokens[0] == 'pow':
-            print(power(num1, num2))
-        if tokens[0] == 'mod':
-            print(mod(num1, num2))
+        if len(tokens)<3:
+            if tokens[0] == 'square':
+                print(square(num1))
+            if tokens[0] == 'cube':
+                print(cube(num1))
+        if len(tokens) == 3:
+            num2 = int(tokens[2])
+            if tokens[0] == '+':
+                print(add(num1, num2))
+            if tokens[0] == '-':
+                print(subtract(num1, num2))
+            if tokens[0] == '*':
+                print(multiply(num1, num2))
+            if tokens[0] == '/':
+                print(divide(num1, num2))
+            if tokens[0] == 'pow':
+                print(power(num1, num2))
+            if tokens[0] == 'mod':
+                print(mod(num1, num2))
